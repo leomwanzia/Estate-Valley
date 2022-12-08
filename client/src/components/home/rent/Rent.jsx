c6ac682fbea83bec20171833a6a27c521634c1e6
Leo <leomutungi19@gmail.com>
2022-12-09 02:54:12 +0300

    background

--- client/src/components/home/rent/Rent.jsx
+++ client/src/components/home/rent/Rent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Heading from "../../head/Heading"
 import "./rent.css"
 
 const Rent = () => {
@@ -6,10 +7,21 @@ const Rent = () => {
         <>
             <section className="rent">
                 <div className="container">
-                    <form action="" className="flex">
+                <Heading title='Move to What Moves You' subtitle='Let us Guide you Home' />
+                    <form className="flex">
                         <div className="box">
                             <span>City/Town</span>
                             <input type="text" placeholder="Location" />
+                        </div>
+
+                        <div className="box">
+                            <span>Price of Properties</span>
+                            <input type="text" placeholder="Price Range" />
+                        </div>
+
+                        <div className="box">
+                            <span>Property Type</span>
+                            <input type="text" placeholder="Property Type" />
                         </div>
                     </form>
                 </div>
