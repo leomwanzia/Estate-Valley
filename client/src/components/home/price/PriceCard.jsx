8f9ca9d9e9075e75adeb849ada276dff4d2e877b
Leo <leomutungi19@gmail.com>
2022-12-09 14:32:14 +0300

    price card

--- client/src/components/home/price/PriceCard.jsx
+++ client/src/components/home/price/PriceCard.jsx
@@ -12,6 +12,30 @@ const PriceCard = () => {
                             <div className="topbtn">
                                 <button className="btn3">{item.best}</button>
                             </div>
+                            <h3>{item.plan}</h3>
+                            <h1>
+                                <span>$</span> {item.price}
+                               <p>{item.ptext}</p> 
+
+                               <ul>
+                               {item.list.map((val) => {
+                                 const { icon, text, change } = val
+                                 return (
+                                   <li>
+                                     <label
+                                       style={{
+                                         background: change === "color" ? "#dc35451f" : "#27ae601f",
+                                         color: change === "color" ? "#dc3848" : "#27ae60",
+                                       }}
+                                     >
+                                       {icon}
+                                     </label>
+                                     <p>{text}</p>
+                                   </li>
+                                 )
+                               })}
+                             </ul>
+                            </h1>
                         </div>
                     )
                 })}
