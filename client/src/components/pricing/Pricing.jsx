afd257a28a079fef5596fdb591b97987233214ee
Leo <leomutungi19@gmail.com>
2022-12-11 11:05:47 +0300

    pricing

---
+++ client/src/components/pricing/Pricing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pricing = () => { 
+    return (
+
+        <>
+            <section className="services mb"
+                <Back name = 'services' title="Services -All Services" cover={img} />
+        </>
+    )
+}
